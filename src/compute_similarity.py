dd992f9939d7d917dc3f800f562455c15f07c9bb
Rainyel Ramos <rainyel.ramos@gmail.com>
2022-05-11 19:35:59 -0400

    move orb code to a function

--- src/compute_similarity.py
+++ src/compute_similarity.py
@@ -5,6 +5,20 @@ from sewar.full_ref import msssim, scc
 import ssim as pyssim
 from matplotlib import pyplot as plt
 
+def orb(image_a, image_b, presicion: int=50)->float:
+    orb = cv2.ORB_create()
+    kp1, des1 = orb.detectAndCompute(imageA, None)
+    kp2, des2 = orb.detectAndCompute(imageB, None)
+
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+
+                # knnFilter results
+    matches = bf.match(des1, des2)
+
+                # View the maximum number of matching points
+    good = [m for m in matches if m.distance < presicion]
+    return len(good) / len(matches)
+
 absolute_path_imageA = "/home/rainyel/Documents/datamart/image-comparison/src/output/image_a.jpg"
 absolute_path_imageB = "/home/rainyel/Documents/datamart/image-comparison/src/output/image_b.jpg"
 
@@ -23,23 +37,11 @@ sre_value = sre(imageA, imageB)
 ssim_value = ssim(grayA, grayB)
 msssim_value = msssim(grayA, grayB)
 dssim_value = (1-ssim_value) / 2
-
+orb_value = orb(grayA, grayB)
 # descriptor = cv2.xfeatures2d.BEBLID_create(0.75)
-orb = cv2.ORB_create()
-kp1, des1 = orb.detectAndCompute(imageA, None)
-kp2, des2 = orb.detectAndCompute(imageB, None)
 
-bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+BRISK = cv2.BRISK_create()
 
-            # knnFilter results
-matches = bf.match(des1, des2)
-
-            # View the maximum number of matching points
-good = [m for m in matches if m.distance < 90]
-print(len(good))
-print(len(matches))
-similary = len(good) / len(matches)
-print(similary)
 # NOTE - the process is killed
 # cw_ssim_value = pyssim.SSIM(absolute_path_imageA).cw_ssim_value(absolute_path_imageB)
 
@@ -52,6 +54,7 @@ print(f"PSNR: {psnr_value}")
 print(f"SRE: {sre_value}")
 print(f"ISSM: {issm_value}")
 print(f"SCC: {scc_value}")
+print(f"ORB: {orb_value}")
 # print(f"CW-SSIM: {cw_ssim_value}")
 # print(f"FSIM: {fsim_value}")
 
