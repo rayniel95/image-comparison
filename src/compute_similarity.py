320181b003d1226b2661808447bae429f191ed6a
Rainyel Ramos <rainyel.ramos@gmail.com>
2022-05-11 20:06:18 -0400

    split code to utility function and add brisk

--- src/compute_similarity.py
+++ src/compute_similarity.py
@@ -5,19 +5,29 @@ from sewar.full_ref import msssim, scc
 import ssim as pyssim
 from matplotlib import pyplot as plt
 
-def orb(image_a, image_b, presicion: int=50)->float:
-    orb = cv2.ORB_create()
-    kp1, des1 = orb.detectAndCompute(imageA, None)
-    kp2, des2 = orb.detectAndCompute(imageB, None)
 
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+def compute_similarity_with_descriptors(
+    algorithm, image_a, image_b, presicion: int=50, norm: int=cv2.NORM_HAMMING
+)->float:
+    kp1, des1 = algorithm.detectAndCompute(image_a, None)
+    kp2, des2 = algorithm.detectAndCompute(image_b, None)
 
-                # knnFilter results
+    bf = cv2.BFMatcher(norm)
+    # knnFilter results
     matches = bf.match(des1, des2)
-
-                # View the maximum number of matching points
+    # View the maximum number of matching points
     good = [m for m in matches if m.distance < presicion]
     return len(good) / len(matches)
+
+def orb(image_a, image_b, presicion: int=50)->float:
+    return compute_similarity_with_descriptors(
+        cv2.ORB_create(), image_a, image_b, presicion
+    )
+
+def brisk(image_a, image_b, presicion: int=50)->float:
+    return compute_similarity_with_descriptors(
+        cv2.BRISK_create(), image_a, image_b, presicion
+    )
 
 absolute_path_imageA = "/home/rainyel/Documents/datamart/image-comparison/src/output/image_a.jpg"
 absolute_path_imageB = "/home/rainyel/Documents/datamart/image-comparison/src/output/image_b.jpg"
@@ -38,9 +48,9 @@ ssim_value = ssim(grayA, grayB)
 msssim_value = msssim(grayA, grayB)
 dssim_value = (1-ssim_value) / 2
 orb_value = orb(grayA, grayB)
+brisk_value = brisk(grayA, grayB)
 # descriptor = cv2.xfeatures2d.BEBLID_create(0.75)
 
-BRISK = cv2.BRISK_create()
 
 # NOTE - the process is killed
 # cw_ssim_value = pyssim.SSIM(absolute_path_imageA).cw_ssim_value(absolute_path_imageB)
@@ -55,6 +65,7 @@ print(f"SRE: {sre_value}")
 print(f"ISSM: {issm_value}")
 print(f"SCC: {scc_value}")
 print(f"ORB: {orb_value}")
+print(f"BRISK: {brisk_value}")
 # print(f"CW-SSIM: {cw_ssim_value}")
 # print(f"FSIM: {fsim_value}")
 
